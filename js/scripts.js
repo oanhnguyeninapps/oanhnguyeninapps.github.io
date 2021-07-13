64765b7ff1a5e11e1d3cae899396a7e93b06be61
oanhnguyeninapps <87379545+oanhnguyeninapps@users.noreply.github.com>
2021-07-14 00:00:11 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ window.onload = function() {
       'We\'re currently looking for talents new exciting roles in HCMC as below:',
       '<a target="_blank" href="https://inapps.intalents.co/job/project-manager-ho-chi-minh?fbclid=IwAR2TKR5rG4amu90d9oKS8poKJnXrDO4eYaxRQ6TSDpzXlTI8RGcj_UCUgZc">Project Manager</a>',
       '<a target="_blank" href="https://inapps.intalents.co/job/backend-developer-nodejs-ho-chi-minh?fbclid=IwAR15pl0Kalp_1J0pCxRpqFiBEAH4yQE-1WfobDvp1RcykaI39KDJZKZGpYw">BackEnd Developer (NodeJS)</a>',
-      'About us: <a target="_blank" href="https://inapps.intalents.co/job/project-manager-ho-chi-minh?fbclid=IwAR2TKR5rG4amu90d9oKS8poKJnXrDO4eYaxRQ6TSDpzXlTI8RGcj_UCUgZc">InApps Technology</a>',
+      'About us: <a target="_blank" href="https://www.inapps.net/?fbclid=IwAR0laeX5Cj-YNheyB6mghgreDWhLfhaSniIRvEEn9p0eSzIHQ7m04-AK0vk#">InApps Technology</a>',
       'To taking care of your career development and advancement successfully!',
       'Let\'s contact me, I will direct support 👉🏻 <a href="skype:oanhnch.hr?chat">Chat With Me On Skype</a>',
       'Thank you so much.'
